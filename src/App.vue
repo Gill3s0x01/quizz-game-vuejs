67019814479309baca175131755fa23b35a3dcd2
Gilles0x01 <Gill3s0x01@users.noreply.github.com>
2024-09-15 22:02:35 -0300

    feat: Implementing function list questions

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,51 @@
+<!-- eslint-disable vuejs-accessibility/label-has-for -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <template v-if="this.question">
+      <h1 v-html="this.question"></h1>
+      <template v-for="(answer, index) in this.answers" v-bind:key="index" >
+        <input
+          type="radio"
+          name="options"
+          value="answer"
+        >
+        <label v-html="answer"></label><br>
+      </template>
+    </template>
+  </div>
+  <button class="send" type="button">Enviar</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+// import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      question: undefined,
+      correctAnswer: undefined,
+      incorrectAnswer: undefined,
+    };
+  },
+  computed: {
+    answers() {
+      const answers = [...this.incorrectAnswer];
+      answers.splice(Math.round(Math.random()), 0, this.correctAnswer);
+      return answers;
+    },
+  },
+  created() {
+    this.axios.get('https://opentdb.com/api.php?amount=1&category=15&difficulty=medium')
+      .then((response) => {
+        this.question = response.data.results[0].question;
+        this.correctAnswer = response.data.results[0].correct_answer;
+        this.incorrectAnswer = response.data.results[0].incorrect_answers;
+      });
+  },
+
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 };
 </script>
@@ -21,6 +57,28 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px auto;
+  max-width: 960px;
+  input[type=radio] {
+    margin: 12px 4px;
+  }
+  button.send {
+    margin-top: 12px;
+    height: 40px;
+    min-width: 120px;
+    padding: 0 16px;
+    color: #fff;
+    background-color: #1867c0;
+    border: 1px solid #1867c0;
+    border-radius: 5px;
+    cursor: pointer;
+
+    &:hover {
+      background-color: #b9c9d8;
+      border-color: #b9c9d8;
+      color: #1867c0;
+      transition: ease-in-out 0.3s;
+    }
+  }
 }
 </style>
